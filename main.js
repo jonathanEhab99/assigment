2c0385aaf06ab0a82a9a27e33f44070f21d5a179
Jonathan ehab <jokaes1999@gmail.com>
2023-07-14 18:30:43 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+var quotes = [
+    {text: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+     author: "Nelson Mandela"},
+    {text: "The way to get started is to quit talking and begin doing.", 
+    author: "Walt Disney"},
+    {text: "If life were predictable it would cease to be life, and be without flavor.", 
+    author: "Eleanor Roosevelt"},
+    {text: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.", author: "Oprah Winfrey"},
+    {text: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.", author: "James Cameron"},
+    {text: "Life is what happens when you're busy making other plans.",
+     author: "John Lennon"},
+    {text: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+     author: "Mother Teresa"},
+    {text: "When you reach the end of your rope, tie a knot in it and hang on.",
+     author: "Franklin D. Roosevelt"},
+    {text: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+     author: "Helen Keller"},
+    {text: "It is during our darkest moments that we must focus to see the light.",
+     author: "Aristotle"},
+  ];
+  function generateQuote() {
+    var randomIndex = Math.floor(Math.random() * quotes.length);
+    var randomQuote = quotes[randomIndex];
+    document.getElementById("quote-text").innerHTML = randomQuote.text;
+    document.getElementById("quote-author").innerHTML= "- " + randomQuote.author;
+  }
